a033d06d086f4b18ad812c496d21c7c54bbea53a
NaingLynnAung <unrivaledking1@gmail.com>
2022-07-25 17:01:10 +0630

    learn migration

--- learn-laravel-basic/routes/web.php
+++ learn-laravel-basic/routes/web.php
@@ -14,70 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Redirect
-
-Route::redirect('/here', '/there');
-
-// View
-
-Route::view('/view', '/about', ['name'=>'unknow']);
-
-//Require Parameter
-
-Route::get('users/{id}', function ($id) {
-    return 'User'. $id ;
-});
-
-Route::get('post/{postId}/user/{userId}', function ($postId, $userId) {
-    return "This is $postId and User id is $userId";
-});
-
-
-// Optional Parameter
-
-Route::get('customer/{name?}', function ($name = "Customer") {
-    return "Customer name is ". $name;
-});
-
-// Regular Expression
-
-Route::get('client/{id}/{name}', function ($id, $name) {
-    return "Client id is $id and Name is $name";
-})->whereNumber('id')->whereAlpha('name');
-
-Route::get('category/{category}', function ($category) {
-    return $category;
-})->whereIn('category', ['movie', 'song', 'painting']);
-
-// Name routes
-
-// Route::get('home', function () {
-//     return view('home');
-// })->name('home');
-
-Route::get('/about-me', function () {
-    return view("about");
-})->name('about');
-
-Route::get('user/{id?}/profile', function ($id) {
-    return view('user', ['id'=>$id]);
-})->name('profile');
-
-// Route Prefix
-
-Route::prefix('admin')->group(function(){
-    Route::get('/home', function () {
-        return view('home');
-    });
-});
-
-Route::name('admin.')->group(function(){
-    Route::get('/user', function () {
-        return view('user');
-    })->name('user');
+Route::get('/table', function () {
+    if(Schema::hasTable('contacts')) {
+        echo "Table exists";
+    }else{
+        echo "Table doesn't exit";
+    };
+    if (Schema::hasColumn('contacts', 'status')) {
+        echo 'Column exists';
+    }else{
+        echo "Column doesn't exit";
+    };
 });
 
